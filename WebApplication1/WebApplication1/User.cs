e4ffdbb27dbe40f63ddaaa6fdc4d83081d1f3a16
keerthana <keerthananeeluru2001@gmail.com>
2023-09-14 10:57:01 +0530

    adding day5 webapp

---
+++ WebApplication1/WebApplication1/User.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication1
+{
+    public class User
+    {
+        [Required]
+        public int UserId { get; set; }
+        [Required]
+        public string UserName { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        public string UserPwd { get; set; }
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        public string UserEmail { get; set; }
+        
+
+    }
+}
